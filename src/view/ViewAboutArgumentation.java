a0dd3b491b8af3a0b1e49f9f00d379c4736727d2
unknown <iarlorifederico@gmail.com>
2022-11-22 14:11:56 -0300

    breve explicacion de argumentacion abstracta

--- src/view/ViewAboutArgumentation.java
+++ src/view/ViewAboutArgumentation.java
@@ -14,9 +14,23 @@ public class ViewAboutArgumentation extends javax.swing.JFrame{
     private ControllerMainWindow controllerMainWindow;
 
     private JPanel mainPanel, panelBottom, panelCenter;
-    private JTextArea taAboutArgumentation;
+    private JEditorPane taAboutArgumentation;
     private JScrollPane spAboutArgumentation;
     private JButton btnClose;
+
+    private final String aboutArgumentation = "<h1>Argumentation Frameworks and Extensions</h1>" +
+    "<p>An argumentation framework (AF) is a directed graph, where nodes are <b>arguments</b> and edges are <b>attacks</b> between the arguments.</p>" +
+    "<p>The <b>semantics</b> (procedure for finding winning arguments) of an AF can be expressed by selecting a subset of the arguments, called an extension, that would be the accepted arguments. Those arguments that were not selected are unaccepted or undecided.</p>"+
+    "<p>All semantics are based on the concept of conflict-freenes.</p>"+
+    "<hr><h2>Conflict-free sets</h2>"+
+    "<p> A subset S from the arguments of an AF is conflict-free if there doesn't exists two arguments <em>a</em> and <em>b</em> in S such that <em>a</em> attacks <em>b</em> or <em>a</em> attacks <em>a</em> </p>"+
+    "<hr><h2>Semantics</h2>"+
+    "<p>Given an AF = (Ar, att), then a conflict-free set S from AF is a(an):</p>"+
+    "<ul>"+
+    "<li> <b>Admissible extension</b> if each argument belonging to S is defended by S.</li>"+
+    "<li> <b>Complete extension</b> if S is an admissible extension and for every argument <em>a</em> from Ar defended by S, it holds that <em>a</em> belongs to S.</li>"+
+    "<li> <b>Preferred extension</b> if S is a maximal admissible extension with respect to set inclusion.</li>"+
+    "</ul>";
 
     public ViewAboutArgumentation(ControllerMainWindow controllerMainWindow){
         this.controllerMainWindow = controllerMainWindow;
@@ -45,13 +59,12 @@ public class ViewAboutArgumentation extends javax.swing.JFrame{
         panelCenter.setLayout(new FlowLayout());
         mainPanel.add(panelCenter, BorderLayout.CENTER); */
 
-        //TODO: ver de usar un textArea
-        taAboutArgumentation = new JTextArea();
+        taAboutArgumentation = new JEditorPane("text/html", "");
         taAboutArgumentation.setEditable(false);
-        taAboutArgumentation.setLineWrap(true);
-        taAboutArgumentation.setWrapStyleWord(true);
-        taAboutArgumentation.setFont(new Font("Calibri", Font.PLAIN, 16));
-        taAboutArgumentation.setText("a ver q pasaa ver q pasaa ver q pasaa ver q pasaa ver q pasaa ver q pasaa ver q pasaa ver q pasaa ver q pasaa ver q pasaa ver q pasaa ver q pasaa ver q pasaa ver q pasaa ver q pasaa ver q pasa");
+        //taAboutArgumentation.setLineWrap(true);
+        //taAboutArgumentation.setWrapStyleWord(true);
+        taAboutArgumentation.setFont(new Font("Calibry", Font.PLAIN, 17));
+        taAboutArgumentation.setText(aboutArgumentation);
         spAboutArgumentation = new JScrollPane(taAboutArgumentation);
         spAboutArgumentation.setSize(new Dimension(800, 400));
         mainPanel.add(spAboutArgumentation, BorderLayout.CENTER);
